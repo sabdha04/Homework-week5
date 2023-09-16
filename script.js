30ab6c356bb5114d64861261284c2f51756c0d05
sabdha04 <sabdhacipaku04@gmail.com>
2023-09-16 19:45:05 +0700

    update fie script.js ditambahkan isi untuk membuat hasil isian dari form registrasi ke tabel dihalaman list pendaftar

--- script.js
+++ script.js
@@ -17,3 +17,75 @@ function openTab(event, tabName) { //fungsi disini itu untuk membuka halaman dar
     document.getElementById(tabName).style.display = 'block';
     event.currentTarget.classList.add('active');
 }
+
+function submitForm() { //fungsi untuk menambahkan isian data dari form registrasi ke tabel dihalaman list pendaftar 
+    const nama = document.getElementById('nama').value;
+    const umur = parseInt(document.getElementById('umur').value);
+    const uangSangu = parseInt(document.getElementById('uangSangu').value);
+
+    if (nama.length >= 10 && umur >= 25 && uangSangu >= 100000 && uangSangu <= 1000000) {
+        pendaftarList.push({ nama, umur, uangSangu });
+        updatePendaftarTable();
+        rataRata();
+        clearForm();
+    } else {
+        alert('Data tidak valid. Pastikan Nama minimal 10 karakter, Umur minimal 25 tahun, Uang Sangu minimal 100 ribu dan maksimal 1 juta.');
+    }
+}
+
+function updatePendaftarTable() {
+    const tableBody = pendaftarTable.querySelector('tbody');
+    tableBody.innerHTML = '';
+
+    pendaftarList.forEach(pendaftar => {
+        const newRow = tableBody.insertRow();
+        const cellNama = newRow.insertCell(0);
+        const cellUmur = newRow.insertCell(1);
+        const cellUangSangu = newRow.insertCell(2);
+
+        cellNama.innerHTML = pendaftar.nama;
+        cellUmur.innerHTML = pendaftar.umur;
+        cellUangSangu.innerHTML = pendaftar.uangSangu;
+    });
+}
+
+function rataRata() { //fungsi untuk menghitung rata-rata uang sangu dan umur 
+    const totalUangSangu = pendaftarList.reduce((total, pendaftar) => total + pendaftar.uangSangu, 0);
+    const totalUmur = pendaftarList.reduce((total, pendaftar) => total + pendaftar.umur, 0);
+
+    const averageUangSangu = totalUangSangu / pendaftarList.length;
+    const averageUmur = totalUmur / pendaftarList.length;
+
+    avgUangSangu.textContent = averageUangSangu.toFixed();
+    avgUmur.textContent = averageUmur.toFixed();
+}
+
+function clearForm() { //fungsi untuk menghapus isian dari form registrasi setelah mengklik button submit
+    document.getElementById('nama').value = '';
+    document.getElementById('umur').value = '';
+    document.getElementById('uangSangu').value = '';
+}
+
+function deletePendaftar(index) { //fungsi untuk menghapus data dari tabel list pendaftar dengan mengklik button X
+    pendaftarList.splice(index, 1);
+    updatePendaftarTable();
+    rataRata();
+}
+
+function updatePendaftarTable() {
+    const tableBody = pendaftarTable.querySelector('tbody');
+    tableBody.innerHTML = '';
+
+    pendaftarList.forEach((pendaftar, index) => {
+        const newRow = tableBody.insertRow();
+        const cellNama = newRow.insertCell(0);
+        const cellUmur = newRow.insertCell(1);
+        const cellUangSangu = newRow.insertCell(2);
+        const cellDelete = newRow.insertCell(3);
+
+        cellNama.innerHTML = pendaftar.nama;
+        cellUmur.innerHTML = pendaftar.umur;
+        cellUangSangu.innerHTML = pendaftar.uangSangu;
+        cellDelete.innerHTML = `<button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline tablinks" onclick="deletePendaftar(${index})">X</button>`;
+    });
+}
